da38c1f90bc39c4a9c87f7b1538a2a67eba7cd8b
Charlie Chrisman <charlie.chrisman@gmail.com>
2019-02-28 15:34:25 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+export const deliveryAccessToken = '0b9374eaed08e76546e4ff945a39631994209bbca4c80e870ce42b82fd5a9032';
+export const spaceId = 'xoeb1r5gsguu';
+
+export const galleryTypeId = 'photoGallery';
